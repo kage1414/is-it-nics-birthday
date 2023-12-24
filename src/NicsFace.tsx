5d46669774b42e0878886e0bbac644139c5612b4
Kyle Johnson <kylejohnson92294@gmail.com>
2023-12-23 21:12:10 -0500

    inplicit return

--- src/NicsFace.tsx
+++ src/NicsFace.tsx
@@ -2,16 +2,14 @@ interface Props {
   mouseCoordinates: number[];
 }
 
-export const NicsFace = ({ mouseCoordinates }: Props) => {
-  return (
-    <img
-      src="src/assets/nics-face.png"
-      style={{
-        position: "absolute",
-        top: mouseCoordinates[0] - 40,
-        left: mouseCoordinates[1] - 40,
-        zIndex: -1,
-      }}
-    />
-  );
-};
+export const NicsFace = ({ mouseCoordinates }: Props) => (
+  <img
+    src="src/assets/nics-face.png"
+    style={{
+      position: "absolute",
+      top: mouseCoordinates[0] - 40,
+      left: mouseCoordinates[1] - 40,
+      zIndex: -1,
+    }}
+  />
+);
