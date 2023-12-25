0589e3864bb2fdba8b70295e51667c78687140e2
kage1414 <kylejohnson92294@gmail.com>
2023-12-24 23:49:25 -0500

    adjust orbit speed

--- src/NicsFace.tsx
+++ src/NicsFace.tsx
@@ -1,9 +1,41 @@
+import { useCallback, useState } from "react";
+
 interface Props {
   mouseCoordinates: number[];
   clicked: boolean;
 }
 
 export const NicsFace = ({ mouseCoordinates, clicked }: Props) => {
+  const [speedAlteration, setSpeedAlteration] = useState(0);
+
+  window.document.body.onkeydown = (e) => {
+    if (e.key === "ArrowUp") {
+      const newSpeed = Number((speedAlteration - 0.05).toFixed(2));
+      setSpeedAlteration(newSpeed);
+    } else if (e.key === "ArrowDown") {
+      const newSpeed = Number((speedAlteration + 0.05).toFixed(2));
+      setSpeedAlteration(newSpeed);
+    }
+  };
+
+  const getImageStyles = useCallback(
+    ({
+      time,
+      orbit,
+    }: {
+      time: number;
+      orbit: "medOrbit" | "narrowOrbit" | "wideOrbit";
+    }) => ({
+      MozAnimation: `${orbit} ${time}s linear infinite`,
+      WebkitAnimation: `${orbit} ${time}s linear infinite`,
+      OAnimation: `${orbit} ${time}s linear infinite`,
+      animation: `${orbit} ${time}s linear infinite`,
+      borderRadius: "5px",
+      height: "48px",
+    }),
+    []
+  );
+
   return (
     <span
       style={{
@@ -18,46 +50,38 @@ export const NicsFace = ({ mouseCoordinates, clicked }: Props) => {
       }}
     >
       <img
-        style={{
-          MozAnimation: `medOrbit 1.5s linear infinite`,
-          WebkitAnimation: `medOrbit 1.5s linear infinite`,
-          OAnimation: `medOrbit 1.5s linear infinite`,
-          animation: `medOrbit 1.5s linear infinite`,
-          borderRadius: "5px",
-          height: "48px",
-          animationPlayState: clicked ? "running" : "paused",
-          MozAnimationPlayState: clicked ? "running" : "paused",
-          WebkitAnimationPlayState: clicked ? "running" : "paused",
-          OAnimationPlayState: clicked ? "running" : "paused",
+        style={getImageStyles({
+          orbit: "medOrbit",
+          time: 1.5 + speedAlteration,
+        })}
+        ref={(ref) => {
+          if (ref) {
+            ref.style.animationPlayState = clicked ? "running" : "paused";
+          }
         }}
         src="/src/assets/nics-face.png"
       />
       <img
-        style={{
-          MozAnimation: `narrowOrbit 0.9s linear infinite`,
-          WebkitAnimation: `narrowOrbit 0.9s linear infinite`,
-          OAnimation: `narrowOrbit 0.9s linear infinite`,
-          animation: `narrowOrbit 0.9s linear infinite`,
-          borderRadius: "5px",
-          height: "36px",
-          animationPlayState: clicked ? "running" : "paused",
-          MozAnimationPlayState: clicked ? "running" : "paused",
-          WebkitAnimationPlayState: clicked ? "running" : "paused",
-          OAnimationPlayState: clicked ? "running" : "paused",
+        style={getImageStyles({
+          orbit: "narrowOrbit",
+          time: 0.9 + speedAlteration,
+        })}
+        ref={(ref) => {
+          if (ref) {
+            ref.style.animationPlayState = clicked ? "running" : "paused";
+          }
         }}
         src="/src/assets/nic-smirking.png"
       />
       <img
-        style={{
-          MozAnimation: `wideOrbit 2s linear infinite`,
-          WebkitAnimation: `wideOrbit 2s linear infinite`,
-          OAnimation: `wideOrbit 2s linear infinite`,
-          animation: `wideOrbit 2s linear infinite`,
-          height: "48px",
-          animationPlayState: clicked ? "running" : "paused",
-          MozAnimationPlayState: clicked ? "running" : "paused",
-          WebkitAnimationPlayState: clicked ? "running" : "paused",
-          OAnimationPlayState: clicked ? "running" : "paused",
+        style={getImageStyles({
+          orbit: "wideOrbit",
+          time: 2 + speedAlteration,
+        })}
+        ref={(ref) => {
+          if (ref) {
+            ref.style.animationPlayState = clicked ? "running" : "paused";
+          }
         }}
         src="/src/assets/nic-staring.png"
       />
