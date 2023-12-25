dbf0907ae8deba6f85542052b06cc3c5f587bf88
kage1414 <kylejohnson92294@gmail.com>
2023-12-24 23:08:12 -0500

    add way too much shit

--- src/NicsFace.tsx
+++ src/NicsFace.tsx
@@ -1,51 +1,52 @@
-import { useCallback, useEffect, useState } from "react";
-import { getWindowDimensions } from "./utils";
-
 interface Props {
   mouseCoordinates: number[];
 }
 
 export const NicsFace = ({ mouseCoordinates }: Props) => {
-  const [screenWidth, setScreenWidth] = useState(getWindowDimensions().width);
-  const [screenHeight, setScreenHeight] = useState(
-    getWindowDimensions().height
-  );
-
-  const handleResize = useCallback(() => {
-    setScreenWidth(getWindowDimensions().width);
-    setScreenHeight(getWindowDimensions().height);
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [handleResize]);
-
-  const height = screenWidth / 6.3 > 120 ? 120 : screenWidth / 6.3;
-
-  const rotationSpeed = (screenHeight - mouseCoordinates[0]) / 230;
-
-  console.log(rotationSpeed);
-
   return (
     <span
-      className="nics-face"
       style={{
         position: "absolute",
-        top: mouseCoordinates[0] - height / 2,
-        left: mouseCoordinates[1] - height / 2,
-        zIndex: -1,
-        height,
+        top: "0px",
+        left: "0px",
+        transform: `translate(${mouseCoordinates[1]}px, ${mouseCoordinates[0]}px)`,
+        transition: "800ms",
       }}
     >
       <img
         style={{
-          MozAnimation: `rotatay ${rotationSpeed}s linear infinite`,
-          WebkitAnimation: `rotatay ${rotationSpeed}s linear infinite`,
-          OAnimation: `rotatay ${rotationSpeed}s linear infinite`,
-          animation: `rotatay ${rotationSpeed}s linear infinite`,
+          zIndex: -1,
+          MozAnimation: `medOrbit 1.5s linear infinite`,
+          WebkitAnimation: `medOrbit 1.5s linear infinite`,
+          OAnimation: `medOrbit 1.5s linear infinite`,
+          animation: `medOrbit 1.5s linear infinite`,
+          borderRadius: "5px",
+          height: "48px",
         }}
-        src="nics-face.png"
+        src="/src/assets/nics-face.png"
+      />
+      <img
+        style={{
+          zIndex: -1,
+          MozAnimation: `narrowOrbit 0.75s linear infinite`,
+          WebkitAnimation: `narrowOrbit 0.75s linear infinite`,
+          OAnimation: `narrowOrbit 0.75s linear infinite`,
+          animation: `narrowOrbit 0.75s linear infinite`,
+          borderRadius: "5px",
+          height: "36px",
+        }}
+        src="/src/assets/nic-smirking.png"
+      />
+      <img
+        style={{
+          zIndex: -1,
+          MozAnimation: `wideOrbit 2s linear infinite`,
+          WebkitAnimation: `wideOrbit 2s linear infinite`,
+          OAnimation: `wideOrbit 2s linear infinite`,
+          animation: `wideOrbit 2s linear infinite`,
+          height: "48px",
+        }}
+        src="/src/assets/nic-staring.png"
       />
     </span>
   );
